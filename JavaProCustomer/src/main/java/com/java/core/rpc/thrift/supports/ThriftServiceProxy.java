d017ade9a6085ca5b1b1bf4817508cdad3dc4a45
庄杰森 <234778286zjs@sina.com>
2017-03-11 23:00:37 +0800

    添加代理类缓存

--- JavaProCustomer/src/main/java/com/java/core/rpc/thrift/supports/ThriftServiceProxy.java
+++ JavaProCustomer/src/main/java/com/java/core/rpc/thrift/supports/ThriftServiceProxy.java
@@ -1,15 +1,32 @@
 package com.java.core.rpc.thrift.supports;
 
 import java.lang.reflect.Proxy;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class ThriftServiceProxy {
 	
+	private static ConcurrentHashMap<String,Object> thriftClientCache;
+	
 	
 	public static <T> T getThriftClient(Class<T> clazz){
+		if (!clazz.isInterface()) {
+			throw new RuntimeException("类型错误");
+		}
+		if (thriftClientCache == null) {
+			thriftClientCache = new ConcurrentHashMap<String,Object>();
+		}
 		T client =null;
-		ThriftServiceProxyInvocation proxyInvocation =new ThriftServiceProxyInvocation();
-		proxyInvocation.setIfaceClazz(clazz);
-		client = (T) Proxy.newProxyInstance(clazz.getClassLoader(), new Class[]{ clazz }, proxyInvocation);
+		String cacheKey = clazz.getName();
+		if (thriftClientCache.containsKey(cacheKey)) {
+			client=(T)thriftClientCache.get(cacheKey);
+		} else {
+			ThriftServiceProxyInvocation proxyInvocation =new ThriftServiceProxyInvocation();
+			proxyInvocation.setIfaceClazz(clazz);
+			client = (T) Proxy.newProxyInstance(clazz.getClassLoader(), new Class[]{ clazz }, proxyInvocation);
+			if (client != null) {
+				thriftClientCache.put(cacheKey, client);
+			}
+		}
 		return client;	
 	}
 
